c704c095f2ea526e1718b74cfed58de65891afad
Srikethanreddyganji <Srikethanreddyganji@gmail.com>
2026-01-10 15:23:29 +0530

    Updated layout, styles, and image

--- html-css-js/web-app-1/script.js
+++ html-css-js/web-app-1/script.js
@@ -1,35 +1,50 @@
+const users = [];
 function dispMessage() {
-  // a=10
-  // b=20
-  // c=a+b
-  //alert("Hello")
-  // alert(document.getElementById("txtEmail").value)
-  // lblMessage.innerHTML = document.getElementById("txtEmail").value + "-" + document.getElementById("txtPassword").value
   let email = document.getElementById("txtEmail").value;
-  let password = document.getElementById("txtPassword").value
-  if (email === "john@gmail.com" && password === "1234") {
+  let password = document.getElementById("txtPassword").value;
+  let found = users.find(
+    (element) => element.email === email && element.password === password
+  );
+  if (found) {
     lblMessage.innerHTML = "Welcome";
   } else {
     lblMessage.innerHTML = "Access Denied";
   }
 }
+function addUser() {
+  let name = document.getElementById("txtName").value;
+  let email = document.getElementById("txtEmail").value;
+  let password = document.getElementById("txtPassword").value;
+  let user = { name: name, email: email, password: password };
+  users.push(user);
+  showLoginForm();
+  console.log(users);
+}
 
-function showLoginForm(){
-    let str = `
+function showLoginForm() {
+  let str = `
     <h3>Login Form</h3>
       <p><label id="lblMessage"></label></p>
-      <p><input type="text" id="txtEmail" /></p>
-      <p><input type="password" id="txtPassword" /></p>
+      <p><input type="text" id="txtEmail" placeholder="Email Address" /></p>
+      <p><input type="password" id="txtPassword" placeholder="Password" /></p>
       <p><button class="login-btn" onclick="dispMessage()">Log In</button></p>
       <hr>
       <p><button class="register-btn" onclick="showRegisterForm()">Create Account</button></p>
-    `
-    root.innerHTML = str
+    `;
+  root.innerHTML = str;
 }
 
-function showRegisterForm(){
-    let str =  `
+function showRegisterForm() {
+  let str = `
      <h3>Registration Form</h3>
-    `
-    root.innerHTML = str
+     <p><label id="lblMessage"></label></p>
+     <p><input type="text" id="txtName" placeholder="Enter Name" /></p>
+      <p><input type="text" id="txtEmail" placeholder="Email Address" /></p>
+      <p><input type="password" id="txtPassword" placeholder="Password" /></p>
+      <p><button class="login-btn" onclick="addUser()">Submit</button></p>
+      <hr>
+      <p>Already a member?<a href="#" onclick="showLoginForm()">Login here</a></p>
+
+    `;
+  root.innerHTML = str;
 }
